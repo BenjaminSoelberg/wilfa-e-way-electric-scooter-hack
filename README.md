7c07730ec304e6d1dc9124f151f387a617cfa3d0
Benjamin Sølberg <benjamin.soelberg@gmail.com>
2021-07-16 23:45:44 +0200

    Added the BLE stack patch and startup of local webserver to aid the installation

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # wilfa-e-way-electric-scooter-hack
-E-way Electric Scooter Speed Hack
+Wilfa E-way Electric Scooter Speed Hack for Android
+
+Will set the speed to unlimited even so the settings page says ie 20 km/t or what ever is the legal max in your country.
+
+Just run patch_apk.sh to patch the original apk and install the resulting apk on your android phone.
+
+To enable unlimited mode, goto settings and press confirm.
+
+Tested on the E-way E350 which has a maximum speed of about 30 km/t
+
+Please use the scooter responsibly and only in legal places.
+
+Always wear a helmet!
+
+https://www.boredpanda.com/reasons-why-wearing-helmet-is-important/
--- patch_apk.sh
+++ patch_apk.sh
@@ -2,6 +2,8 @@
 rm -rf android.jks
 rm -rf EWay_Scooter_v1.0.15_speed
 keytool -genkey -keystore android.jks -keyalg RSA -keysize 2048 -validity 3650 -alias app -dname "cn=Unknown, ou=Unknown, o=Unknown, c=Unknown" -storepass abcdef12 -keypass abcdef12
-apktool d -f -r Eway_Scooter_v1.0.15.apk -o EWay_Scooter_v1.0.15_speed
+apktool d -f -r Eway_Scooter_v1.0.15_original.apk -o EWay_Scooter_v1.0.15_speed
+patch EWay_Scooter_v1.0.15_speed/smali/com/scooter/ewayscooter/Utils/ConstantBle.smali < max_speed.patch
 apktool b EWay_Scooter_v1.0.15_speed -o EWay_Scooter_v1.0.15_speed.apk
 apksigner sign --ks android.jks --ks-pass pass:abcdef12 EWay_Scooter_v1.0.15_speed.apk
+python3 -m http.server 8000
