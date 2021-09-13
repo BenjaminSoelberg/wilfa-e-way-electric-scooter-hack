abb17648bbfdbcbd2f03e95b74c75bab6b717d28
Benjamin Sølberg <benjamin.soelberg@gmail.com>
2021-09-13 20:49:53 +0200

    Hopefully this fixes issue #1

--- apply_patch.sh
+++ apply_patch.sh
@@ -2,6 +2,6 @@
 rm -rf EWay_Scooter_v1.0.15_speed EWay_Scooter_v1.0.15_speed.apk
 cp -r EWay_Scooter_v1.0.15_original EWay_Scooter_v1.0.15_speed
 patch -s -p0 < max_speed.patch
-apktool b EWay_Scooter_v1.0.15_speed -o EWay_Scooter_v1.0.15_speed.apk
+java -jar apktool_2.6.0.jar b EWay_Scooter_v1.0.15_speed -o EWay_Scooter_v1.0.15_speed.apk
 apksigner sign --ks android.jks --ks-pass pass:abcdef12 EWay_Scooter_v1.0.15_speed.apk
 python3 -m http.server 8000
--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@
 rm -rf android.jks
 rm -rf EWay_Scooter_v1.0.15_original
 keytool -genkey -keystore android.jks -keyalg RSA -keysize 2048 -validity 3650 -alias app -dname "cn=Unknown, ou=Unknown, o=Unknown, c=Unknown" -storepass abcdef12 -keypass abcdef12
-apktool d -f -r Eway_Scooter_v1.0.15_original.apk -o EWay_Scooter_v1.0.15_original
+java -jar apktool_2.6.0.jar d -f Eway_Scooter_v1.0.15_original.apk -o EWay_Scooter_v1.0.15_original
 
