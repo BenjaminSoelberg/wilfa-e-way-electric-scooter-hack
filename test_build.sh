ea064488ac0134b53c7d0072f8fbd3015bf45e45
Benjamin Sølberg <benjamin.soelberg@gmail.com>
2021-09-13 22:24:17 +0200

    fixed missing jar reference

--- test_build.sh
+++ test_build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 rm -rf EWay_Scooter_v1.0.15_speed.apk
-apktool b EWay_Scooter_v1.0.15_speed -o EWay_Scooter_v1.0.15_speed.apk
+java -jar apktool_2.6.0.jar b EWay_Scooter_v1.0.15_speed -o EWay_Scooter_v1.0.15_speed.apk
 apksigner sign --ks android.jks --ks-pass pass:abcdef12 EWay_Scooter_v1.0.15_speed.apk
 python3 -m http.server 8000
